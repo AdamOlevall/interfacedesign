f4abbeea6eecf73d6d4d7a2d25d4a5c52d14dbde
stinarichard <a.stina.richard@gmail.com>
2015-04-01 14:51:15 +0200

    leka mer med if

--- if.js
+++ if.js
@@ -1,4 +1,61 @@
-var c = document.getElementById("myCanvas");
+/*var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
-ctx.fillText("Hello World",10,50);
+ctx.fillText("Hello World",10,50); */
+
+/*
+	// draw the cloud
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d'); 
+
+
+
+      // begin custom shape
+      context.beginPath();
+      context.moveTo(170, 80);
+      context.bezierCurveTo(130, 100, 130, 150, 230, 150);
+      context.bezierCurveTo(250, 180, 320, 180, 340, 150);
+      context.bezierCurveTo(420, 150, 420, 120, 390, 100);
+      context.bezierCurveTo(430, 40, 370, 30, 340, 50);
+      context.bezierCurveTo(320, 5, 250, 20, 250, 50);
+      context.bezierCurveTo(200, 5, 150, 20, 170, 80);
+
+      // complete custom shape
+      context.closePath();
+      context.lineWidth = 5;
+      context.fillStyle = '#8ED6FF';
+      context.fill();
+      context.strokeStyle = 'blue';
+      context.stroke();
+
+
+
+makefish();
+
+ context.beginPath();
+      context.moveTo(0, 380);
+     // context.quadraticCurveTo(100, 300, 200, 380);
+      context.bezierCurveTo(5, 360, 10, 340, 40, 380);
+      context.moveTo(40, 380);
+      context.bezierCurveTo(45, 360, 50, 340, 80, 380);
+      context.moveTo(40, 380);
+
+     // context.closePath();
+      context.lineWidth = 5;
+      context.fillStyle = '#8ED6FF';
+      context.fill();
+      context.strokeStyle = 'white';
+      context.stroke();
+
+
+    
+
+
+
+function makefish() {
+ base_image = new Image();
+  base_image.src = 'guldfisk.png';
+  base_image.onload = function(){
+    context.drawImage(base_image, 100, 100);
+
+} */
